2cecb1aeb098ce26858ed18767cd263eafb00ae7
SleepyHead-Zzzz <matyasjobb@gmail.com>
2026-03-26 13:31:27 +0100

    tar bort adam skit

--- index.js
+++
@@ -1,7 +0,0 @@
-var body = document.querySelector("body");
-while(true) {
-    var btn = document.createElement("input");
-    btn.type = "button";
-    btn.id = "changebtn"
-    body.appendChild(btn)
-}
