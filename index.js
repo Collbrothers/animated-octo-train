296c2e56091bdb7785558552b57a7e4695d3d271
Bobban <ward.oliver@icloud>
2026-03-26 13:46:18 +0100

    Three variables were created, btn, darkmode and lightmode. Btn is defined with getElementById function where the id is changebtn, darkmode and lightmode were defined with false and true respectively. An eventListener was added on the button, it listens for the event click and when it hears the event it runs a function that checks if lightmode is true and if it is it changes what css class which is applied to the body and then sets lightmode to false and darkmode to true. If lightmode isn't true it changes the class applied to body to lightmode and then sets darkmode to false and lightmode to true.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Happy developing ✨')
+const btn = getElementById('changebtn');//Gets the button.
+
+//Creates two variables to see which mode is currently applied.
+let darkmode = false;
+let lightmode = true;
+
+//Adds an eventlistener to the button which listens for the click event.
+btn.addEventlistener('click', function () {
+    //Checks if lightmode is true.
+    if(lightmode) {
+        document.body.classList.add('darkmode');//Changes the bodys applied class to darkmode.
+        //Switches the values of the variables to the opposite.
+        darkmode = true;
+        lightmode = false;
+    }
+    else{
+        document.body.classList.add('lightmode');//Changes the bodys applied class to lightmode.
+        //Switches the values of the variables to the opposite.
+        darkmode = false;
+        lightmode = true;  
+    }
+})
